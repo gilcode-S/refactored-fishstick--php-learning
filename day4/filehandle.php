2fd5fb85749062b2d6d664bd536d48e10f128a95
Gilbert Engalan <gilbertpude2003@gmail.com>
2025-09-18 21:42:49 +0800

    file handling and wrtite in php (upload image)

--- day4/filehandle.php
+++ day4/filehandle.php
@@ -2,18 +2,52 @@
 echo "day 4 of php series <br>";
 // read and write files
 
-$file = 'extras/user.txt';
-if(file_exists($file)){
-    $handle = fopen($file, 'r');
-    $contents = fread($handle, filesize($file));
-    fclose($handle);
+// $file = 'extras/user.txt';
+// if(file_exists($file)){
+//     $handle = fopen($file, 'r');
+//     $contents = fread($handle, filesize($file));
+//     fclose($handle);
 
-    echo $contents;
+//     echo $contents;
+// };
+
+
+if(isset($_POST['submit'])){
+
+    $allowed_ext = array('png', 'jpeg', 'jpg', 'gif');
+
+    if(!empty($_FILES['upload']['name'])){
+        //print_r($_FILES);
+        $file_name = $_FILES['upload']['name'];
+        $file_size = $_FILES['upload']['size'];
+        $file_tmp = $_FILES['upload']['tmp_name'];
+        $target_dir = "uploads/${file_name}";
+
+        // get file extension
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+
+        // validate file extension
+        if(in_array($file_ext, $allowed_ext)){
+            if($file_size <= 8000000) {
+                move_uploaded_file($file_tmp, $target_dir);
+                $message = '<p style="color: green;"> file uploaded</p>';
+            } else {
+                $message = '<p style="color: red;"> The file is to large</p>';
+            }
+        } else {
+            $message = '<p style="color: red;"> Invalid file type</p>';
+        }
+    } else {
+        $message = '<p style="color: red;"> Please choose a file</p>';
+    }
 }
 
-?
+?>
 
-// file upload
+
+
+<!-- file upload -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -21,8 +55,11 @@ if(file_exists($file)){
     <title>Document</title>
 </head>
 <body>
-    <form action="">
-    
+    <?php echo $message ?? null; ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+    Select image to upload
+    <input type="file" name="upload">
+    <input type="submit" name="submit" value="submit">
     </form>
 </body>
 </html>
