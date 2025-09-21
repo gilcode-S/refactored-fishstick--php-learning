4d3fb2ed125e179911be85e13f1cb7640e5888c8
Gilbert Engalan <gilbertpude2003@gmail.com>
2025-09-21 22:33:09 +0800

    1st project :/

---
+++ project/config/database.php
@@ -0,0 +1,13 @@
+<?php
+$db_server = '127.0.0.1:3307';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'php_series';
+
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "You are connected!";
+}
