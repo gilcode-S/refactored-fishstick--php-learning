17ec7a60d21e20bd364a02349e3ab795cb2bc4c6
Gilbert Engalan <gilbertpude2003@gmail.com>
2025-09-22 21:09:58 +0800

    finish basic of php series + connection of php and mysqli

--- project/config/database.php
+++ project/config/database.php
@@ -1,7 +1,7 @@
 <?php
 $db_server = '127.0.0.1:3307';
-$db_user = 'root';
-$db_pass = '';
+$db_user = 'garth';
+$db_pass = '12345';
 $db_name = 'php_series';
 
 $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
